55ade6ff45e51a345ea7beb6f694d9ee9f698b1d
Nilesh Pawar <pawarn987@gmail.com>
2020-06-10 10:53:33 +0530

    added JWT and sign in and login user with role base

--- server.js
+++ server.js
@@ -10,25 +10,42 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
 app.use(bodyParser.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// simple route
 const db = require("./app/models");
+const Role = db.role;
 db.sequelize.sync({ force: true }).then(() => {
     console.log("Drop and re-sync db.");
+    initial();
   });
+
+  function initial() {
+    Role.create({
+      id: 1,
+      name: "user"
+    });
+   
+    Role.create({
+      id: 2,
+      name: "moderator"
+    });
+   
+    Role.create({
+      id: 3,
+      name: "admin"
+    });
+  }
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
 require("./app/routes/turorial.routes")(app);
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
